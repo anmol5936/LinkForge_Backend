65d6aa96783a7df03051acf107c8b69fc134c3db
anmol <mangarajanmol666@gmail.com>
2024-05-01 14:18:11 +0530

    Update CORS origin to https://linkforge666.netlify.app in server.ts

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3001",
+    origin: "https://linkforge666.netlify.app",
     credentials: true,
   })
 );
